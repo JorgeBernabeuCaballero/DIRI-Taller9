f8ac53814e5122b5ed7d877da7e47bad0fba88e7
Jorger Bernabeu <jbc85@gcloud.ua.es>
2025-02-20 20:39:26 +0100

    Deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://jorgebernabeucaballero.github.io/DIRI-Taller9/',
+  build: {
+    outDir: 'docs'
+  }
 })
